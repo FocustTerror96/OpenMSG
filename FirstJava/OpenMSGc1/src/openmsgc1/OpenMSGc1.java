75f34adbaaa2ff808756960fc8a8dec2a76b004f
boomtodoom <dylanmaeers@yahoo.co.uk>
2021-06-28 16:19:01 +0100

    Initial client code
    Added a basic username and ip system and a basic socket connection.

--- FirstJava/OpenMSGc1/src/openmsgc1/OpenMSGc1.java
+++ FirstJava/OpenMSGc1/src/openmsgc1/OpenMSGc1.java
@@ -5,21 +5,46 @@
  */
 package openmsgc1;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.net.*;
+
 /**
  *
  * @author sgmud
  */
 public class OpenMSGc1 {
-
+    Scanner scan = new Scanner(System.in);
+    private String username="";
+    private String IPAddress="";
+    public Socket CSocket;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        OpenMSGc1 client = new OpenMSGc1();
+        client.setup();
     }
     
-    void sendMessage(){
-        System.out.println("hello")
+    void setup(){
+        try{
+            System.out.println("Please enter your desired username: ");
+            username = scan.nextLine();
+            System.out.println("Please enter the ip of your target server: ");
+            IPAddress = scan.nextLine();
+            System.out.println(username+ipAddress);
+        } catch(IOException e) {
+            System.out.println("1");
+        }
+    }
+    
+    void startConnection(){
+        try{
+            CSocket = new Socket(IPAddress, 6666);
+        }catch(IOException e) {
+            System.out.println("1");
+        }
         
     }
 }
+
